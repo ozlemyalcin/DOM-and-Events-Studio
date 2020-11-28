7a5b1e1f1382a10a5c018830eda0cd1f2b6ea4c4
ozlemyalcin <omersam33@gmail.com>
2020-11-27 20:20:21 -0600

    adding new

--- scripts.js
+++ scripts.js
@@ -2,6 +2,8 @@
 // Remember to pay attention to page loading!
 
 function init(){
+    let shuttleheight = document.getElementById("spaceShuttleHeight");
+    let shuttleBackground = document.getElementById("shuttleBackground");
 
     let takeoffbutton = document.getElementById("takeoff");
     takeoffbutton.addEventListener("click",handleTakeoffButton)
@@ -10,61 +12,84 @@ function init(){
     let missionAbort =  document.getElementById("missionAbort");
     missionAbort.addEventListener("click",handleMissionAbort);
 
-   
+    let imjObj=document.getElementById('rocket');
+    imjObj.style.position='relative';
+    imjObj.style.left='0px';
+    imjObj.style.bottom='0px';
 
-function handleTakeoffButton(){
-    let shuttleheight = document.getElementById("spaceShuttleHeight");
-    let shuttleBackground = document.getElementById("shuttleBackground");
+    let right = document.getElementById('right');
+    right.addEventListener("click",function(){
+        movement=parseInt(imjObj.style.left)+10+'px';
+        imjObj.style.left=movement;
 
-    let result = window.confirm("Confirm that the shuttle is ready for takeoff");
-    if (result) {
-        document.querySelector("p").innerHTML="Shuttle in flight.";
-        shuttleBackground.style.backgroundColor = "blue";
-        shuttleheight.innerHTML ="10000"
-    }
+    });
+
+    let left = document.getElementById("left");
+    left.addEventListener("click", function(){
+        movement = parseInt(imjObj.style.left)-10+'px';
+        imjObj.style.left=movement;
+    });
+
+    let down = document.getElementById("down");
+    down.addEventListener("click",function(){
+        movement=parseInt(imjObj.style.bottom)-10+'px';
+        imjObj.style.bottom=movement;
+        shuttleheight.innerHTML=parseInt(shuttleheight.innerHTML)-10000;
+    });
+
+    let up= document.getElementById("up");
+    up.addEventListener("click", function(){
+        movement=parseInt(imjObj.style.bottom)+10+'px';
+        imjObj.style.bottom=movement;
+        shuttleheight.innerHTML=parseInt(shuttleheight.innerHTML)+10000;
+
+    });
+
+
+    function handleTakeoffButton(){
+    
+
+        let result = window.confirm("Confirm that the shuttle is ready for takeoff");
+        if (result) {
+            document.querySelector("p").innerHTML="Shuttle in flight.";
+            shuttleBackground.style.backgroundColor = "blue";
+            shuttleheight.innerHTML ="10000"
+        }
         
     };
 
-function handleLandingButton(){
-    let shuttleheight=document.getElementById("spaceShuttleHeight");
-    let shuttleBackground = document.getElementById("shuttleBackground");
-    let result=window.alert("The shuttle is landing. Landing gear engaged.");
-
-
-    document.querySelector("p").innerHTML="Mission aborted.";
-    shuttleBackground.style.backgroundColor = "green";
-    shuttleheight.innerHTML="0";
-
+    function handleLandingButton(){
     
-};
-
-function handleMissionAbort(){
-    let shuttleheight = document.getElementById("spaceShuttleHeight");
-    let shuttleBackground = document.getElementById("shuttleBackground");
-    let result = window.confirm("Confirm that you want to abort the mission");
-    if(result){
-        document.querySelector("p").innerHTML="mission aborted.";
+        let result=window.alert("The shuttle is landing. Landing gear engaged.");
+        document.querySelector("p").innerHTML="Mission aborted.";
         shuttleBackground.style.backgroundColor = "green";
         shuttleheight.innerHTML="0";
 
-    }
+    
+    };
 
-    
-}
-    
+    function handleMissionAbort(){
    
+        let result = window.confirm("Confirm that you want to abort the mission");
+        if(result){
+            document.querySelector("p").innerHTML="mission aborted.";
+            shuttleBackground.style.backgroundColor = "green";
+            shuttleheight.innerHTML="0";
+
+    };
+
   
 
+    }
 
 handleLandingButton();
 handleTakeoffButton();
 handleMissionAbort();
+
+
 }
- 
     
- 
-   
-    
+
 
 window.onload = init;
     
